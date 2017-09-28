332f4db40a1918729676be51c8ecd418cca83bc8
orpheeantoniadis <orphee.antoniadis@gmail.com>
2017-09-28 20:06:02 +0200

    I can decode assembly from a game.

--- tests/test_gb/gameboy.c
+++ tests/test_gb/gameboy.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "cpu.h"
 
 int main(int argc, char** argv) {
   FILE *cartridge;
-  char instruction;
+  char buffer[30];
+  uint8_t instruction;
+  uint8_t n;
+  uint16_t nn;
 
   if (argc == 2) {
     if ((cartridge = fopen(argv[1],"r")) == NULL) {
@@ -15,8 +19,24 @@ int main(int argc, char** argv) {
       return EXIT_FAILURE;
     }
     while (!feof(cartridge)) {
-      fread(&instruction, sizeof(char), 1, cartridge);
-      printf("%02x ", instruction & 0xff);
+      fread(&instruction, sizeof(uint8_t), 1, cartridge);
+      switch (instructions[(int)instruction].operandLength) {
+        case 0 :
+          printf("%s\n", instructions[(int)instruction].disassembly);
+          break;
+        case 1 :
+          fread(&n, sizeof(uint8_t), 1, cartridge);
+          sprintf(buffer, instructions[(int)instruction].disassembly, n);
+          printf("%s\n", buffer);
+          break;
+        case 2 :
+          fread(&nn, sizeof(uint16_t), 1, cartridge);
+          sprintf(buffer, instructions[(int)instruction].disassembly, nn);
+          printf("%s\n", buffer);
+          break;
+        default :
+          break;
+      }
     }
     fclose(cartridge);
   }
