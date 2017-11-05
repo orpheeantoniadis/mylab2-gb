017a8ed012e51a473851b19263d26b9e700116cc
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2017-11-05 19:38:54 +0100

    [LCD] add LCD status management in lcd_cycle

--- workspace/gb_lib/inc/lcd.h
+++ workspace/gb_lib/inc/lcd.h
@@ -1,19 +1,27 @@
 #ifndef _LCD_H
 #define _LCD_H
 
+#include "memory.h"
 #include <stdint.h>
 
-#define GB_LCDC_WIDTH   160
-#define GB_LCDC_HEIGHT  144
+#define GB_LCD_WIDTH    160
+#define GB_LCD_HEIGHT   144
 #define SCANLINES_NB    153
-#define VBLANK_PERIOD   456
+
+#define LCD_SCAN_PERIOD 456
+#define MODE2_BOUND     80
+#define MODE3_BOUND     252
 
 #define LCDC  0xff40
 #define STAT  0xff41
 #define LY    0xff44
 #define LYC   0xff45
 
-#define LCD_IS_ENABLE() ((read8(LCDC) >> 7) & 1)
+static inline uint8_t LCD_IS_ENABLE() {return (read8(LCDC) >> 7) & 1;}
+static inline void STAT_SET_BIT(uint8_t bit) {write8(STAT,read8(STAT)|(1<<bit));}
+static inline void STAT_CLEAR_BIT(uint8_t bit) {write8(STAT,read8(STAT)&~(1<<bit));}
+static inline void STAT_SET_MODE(uint8_t mode) {write8(STAT,(read8(STAT)&~3)|mode);}
+static inline uint8_t STAT_GET_MODE(void) {return read8(STAT)&3;}
 
 void lcd_cycle(uint8_t cycles);
 
--- workspace/gb_lib/src/lcd.c
+++ workspace/gb_lib/src/lcd.c
@@ -1,20 +1,46 @@
 #include "lcd.h"
-#include "memory.h"
+#include "cpu.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 static uint16_t scanline_counter = 0;
 
 void lcd_cycle(uint8_t cycles) {
-  uint8_t current_line;
-
+  uint8_t ir_selection = 0;
+  uint8_t change_mode = 0;
   if (LCD_IS_ENABLE()) {
     scanline_counter += cycles;
-    if (scanline_counter >= VBLANK_PERIOD) {
-      (memory.MEM[LY])++;
-      current_line = read8(LY);
+    if (scanline_counter < MODE2_BOUND) { // mode 2
+      change_mode = STAT_GET_MODE() != 2;
+      STAT_SET_MODE(2);
+      ir_selection = (read8(STAT) >> 5) & 1;
+    } else if (scanline_counter < MODE3_BOUND) { // mode 3
+      change_mode = STAT_GET_MODE() != 3;
+      STAT_SET_MODE(3);
+    } else if (scanline_counter < LCD_SCAN_PERIOD) { // mode 0
+      change_mode = STAT_GET_MODE() != 0;
+      STAT_SET_MODE(0);
+      ir_selection = (read8(STAT) >> 3) & 1;
+    } else { // mode 1
+      memory.MEM[LY]++;
       scanline_counter = 0;
-      if (current_line > SCANLINES_NB) memory.MEM[LY] = 0;
+      if (read8(LY) > SCANLINES_NB) memory.MEM[LY] = 0;
+      else if (read8(LY) == GB_LCD_HEIGHT) {
+        change_mode = STAT_GET_MODE() != 1;
+        STAT_SET_MODE(1);
+        ir_selection = (read8(STAT) >> 4) & 1;
+        interrupt_request(IR_VBLANK);
+      }
     }
+    if (change_mode && ir_selection) interrupt_request(IR_LCD);
+    if (read8(LY) == read8(LYC)) { // check coincidence flag
+      STAT_SET_BIT(2);
+      if ((read8(STAT) >> 6) & 1) interrupt_request(IR_LCD);
+    } else STAT_CLEAR_BIT(2);
+  } else { // LCD disabled so mode 1 and reset counters
+    scanline_counter = 0;
+    memory.MEM[LY] = 0;
+    STAT_SET_BIT(0);
+    STAT_CLEAR_BIT(1);
   }
 }
