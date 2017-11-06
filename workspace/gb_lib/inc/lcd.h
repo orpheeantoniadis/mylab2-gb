30945c463fe176b2e1f98a944c17739f07f56e47
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2017-11-06 13:01:27 +0100

    [LCD] start line rendering function.

--- workspace/gb_lib/inc/lcd.h
+++ workspace/gb_lib/inc/lcd.h
@@ -12,12 +12,24 @@
 #define MODE2_BOUND     80
 #define MODE3_BOUND     252
 
-#define LCDC  0xff40
-#define STAT  0xff41
-#define LY    0xff44
-#define LYC   0xff45
+#define TILEMAP_LEN           0x400
+#define TILEMAP_STARTREGION0  0x9800
+#define TILEMAP_STARTREGION1  0x9c00
+#define TILEDATA_LEN          0x1000
+#define TILEDATA_STARTREGION0 0x8800
+#define TILEDATA_STARTREGION1 0x8000
 
-static inline uint8_t LCD_IS_ENABLE() {return (read8(LCDC) >> 7) & 1;}
+#define LCDC    0xff40
+#define STAT    0xff41
+#define SCROLLY 0xff42
+#define SCROLLX 0xff43
+#define LY      0xff44
+#define LYC     0xff45
+#define WINDOWY 0xff4a
+#define WINDOWX 0xff4b
+
+static inline uint8_t LCD_IS_ENABLE(void) {return (read8(LCDC)>>7)&1;}
+static inline uint8_t LCDC_BIT_ISSET(uint8_t bit) {return (read8(LCDC)>>bit)&1;}
 static inline void STAT_SET_BIT(uint8_t bit) {write8(STAT,read8(STAT)|(1<<bit));}
 static inline void STAT_CLEAR_BIT(uint8_t bit) {write8(STAT,read8(STAT)&~(1<<bit));}
 static inline void STAT_SET_MODE(uint8_t mode) {write8(STAT,(read8(STAT)&~3)|mode);}
--- workspace/gb_lib/src/lcd.c
+++ workspace/gb_lib/src/lcd.c
@@ -3,34 +3,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static uint16_t scanline_counter = 0;
+static uint16_t cpu_cycles_counter = 0;
+
+static void draw_tiles(void) {
+  uint16_t BG_tilemap_startregion;
+  uint16_t window_tilemap_startregion;
+  uint16_t tiledata_startregion;
+  if (LCDC_BIT_ISSET(3)) BG_tilemap_startregion = TILEMAP_STARTREGION1;
+  else BG_tilemap_startregion = TILEMAP_STARTREGION0;
+  if (LCDC_BIT_ISSET(4)) tiledata_startregion = TILEDATA_STARTREGION1;
+  else tiledata_startregion = TILEDATA_STARTREGION0;
+  if (LCDC_BIT_ISSET(6)) window_tilemap_startregion = TILEMAP_STARTREGION1;
+  else window_tilemap_startregion = TILEMAP_STARTREGION0;
+}
+
+static void draw_sprites(void) {
+  
+}
+
+static void draw_scanline(void) {
+  if (LCDC_BIT_ISSET(0)) {
+    draw_tiles();
+  }
+  if (LCDC_BIT_ISSET(1)) {
+    draw_sprites();
+  }
+}
 
 void lcd_cycle(uint8_t cycles) {
   uint8_t ir_selection = 0;
   uint8_t change_mode = 0;
   if (LCD_IS_ENABLE()) {
-    scanline_counter += cycles;
-    if (scanline_counter < MODE2_BOUND) { // mode 2
+    cpu_cycles_counter += cycles;
+    if (cpu_cycles_counter < MODE2_BOUND) { // mode 2
       change_mode = STAT_GET_MODE() != 2;
       STAT_SET_MODE(2);
       ir_selection = (read8(STAT) >> 5) & 1;
-    } else if (scanline_counter < MODE3_BOUND) { // mode 3
+    } else if (cpu_cycles_counter < MODE3_BOUND) { // mode 3
       change_mode = STAT_GET_MODE() != 3;
       STAT_SET_MODE(3);
-    } else if (scanline_counter < LCD_SCAN_PERIOD) { // mode 0
+    } else if (cpu_cycles_counter < LCD_SCAN_PERIOD) { // mode 0
       change_mode = STAT_GET_MODE() != 0;
       STAT_SET_MODE(0);
       ir_selection = (read8(STAT) >> 3) & 1;
     } else { // mode 1
       memory.MEM[LY]++;
-      scanline_counter = 0;
+      cpu_cycles_counter = 0;
       if (read8(LY) > SCANLINES_NB) memory.MEM[LY] = 0;
       else if (read8(LY) == GB_LCD_HEIGHT) {
         change_mode = STAT_GET_MODE() != 1;
         STAT_SET_MODE(1);
         ir_selection = (read8(STAT) >> 4) & 1;
         interrupt_request(IR_VBLANK);
-      }
+      } else draw_scanline();
     }
     if (change_mode && ir_selection) interrupt_request(IR_LCD);
     if (read8(LY) == read8(LYC)) { // check coincidence flag
@@ -38,7 +63,7 @@ void lcd_cycle(uint8_t cycles) {
       if ((read8(STAT) >> 6) & 1) interrupt_request(IR_LCD);
     } else STAT_CLEAR_BIT(2);
   } else { // LCD disabled so mode 1 and reset counters
-    scanline_counter = 0;
+    cpu_cycles_counter = 0;
     memory.MEM[LY] = 0;
     STAT_SET_BIT(0);
     STAT_CLEAR_BIT(1);
